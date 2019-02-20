4f658e530bdc1efc43bf78ee5f645d3778aa713d
saud9030 <saud.amakn@gmail.com>
2019-02-20 13:34:44 +0300

    Updates

---
+++ precache-manifest.8fdc5554427f9d10d64908133458a674.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a42e1b68a8846f3a611",
+    "url": "/static/css/main.ab796518.chunk.css"
+  },
+  {
+    "revision": "1a42e1b68a8846f3a611",
+    "url": "/static/js/main.1a42e1b6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8fa3261057a4606c044d",
+    "url": "/static/js/2.8fa32610.chunk.js"
+  },
+  {
+    "revision": "9547ed0cab8bda6d6f692aa3b1c3e277",
+    "url": "/static/media/twitter.9547ed0c.svg"
+  },
+  {
+    "revision": "c7978d435230536aade1113647c04e6f",
+    "url": "/static/media/github.c7978d43.svg"
+  },
+  {
+    "revision": "4de961fd2bdefd4a8810b1223cd4d95b",
+    "url": "/static/media/linkedin.4de961fd.svg"
+  },
+  {
+    "revision": "ea95fb60888e52273115af9cf1a5baf4",
+    "url": "/index.html"
+  }
+];
